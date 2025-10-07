de770e31004412594e3095211263ef00579db91f
Jeff Peralta <jaydocperalta@gmail.com>
2025-10-07 13:35:26 +1100

    Init commit

---
+++ contact_form/contact_form.php
@@ -0,0 +1,84 @@
+<?php
+include_once('../../../credentials/Contacts.php');
+
+// configure
+$from = 'admin@jeff-peralta.com'; // Replace it with Your Hosting Admin email. REQUIRED!
+$sendTo = 'admin@jeff-peralta.com'; // Replace it with Your email. REQUIRED!
+$subject = 'New message from contact form';
+$fields = array('name' => 'Name', 'email' => 'Email', 'subject' => 'Subject', 'message' => 'Message'); // array variable name => Text to appear in the email. If you added or deleted a field in the contact form, edit this array.
+$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+$errorMessage = 'There was an error while submitting the form. Please try again later';
+
+// let's do the sending
+
+if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
+    //your site secret key
+    $secret = '6LfCVSkgAAAAALbQfTAct0q1t2gwv6gkZMxw-G0o';
+    //get verify response data
+
+    $c = curl_init('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+    $verifyResponse = curl_exec($c);
+
+    $responseData = json_decode($verifyResponse);
+    if($responseData->success):
+
+        try
+        {
+            $message = "You have new message from Contact Form\r\n";
+
+            foreach ($_POST as $key => $value) {
+                if (isset($fields[$key])) {
+                    $message .= "$fields[$key]: $value\r\n";
+                }
+            }
+
+            $conn = new mysqli($servername, $username, $password, $database);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+        
+            $stmt = $conn->prepare("INSERT INTO `contacts` (`name`, `email`, `subject`, `message`, `status`) VALUES (?, ?, ?, ?, 'NEW')");
+            $stmt->bind_param("ssss", $_POST['name'], $_POST['email'], $_POST['subject'], $message);
+            $stmt->execute();
+            $stmt->close();
+            $conn->close();
+
+
+            $responseArray = array('type' => 'success', 'message' => $okMessage);
+        }
+        catch (\Exception $e)
+        {
+            $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+        }
+
+        if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+            $encoded = json_encode($responseArray);
+
+            header('Content-Type: application/json');
+
+            echo $encoded;
+        }
+        else {
+            echo $responseArray['message'];
+        }
+
+    else:
+        $errorMessage = 'Robot verification failed, please try again.';
+        $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+        $encoded = json_encode($responseArray);
+
+            header('Content-Type: application/json');
+
+            echo $encoded;
+    endif;
+else:
+    $errorMessage = 'Please click on the reCAPTCHA box.';
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+    $encoded = json_encode($responseArray);
+
+            header('Content-Type: application/json');
+
+            echo $encoded;
+endif;
+
